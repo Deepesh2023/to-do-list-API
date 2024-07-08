b0b42f0e9ab6854d020db6395a7c4797c8f01e55
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-07-08 12:51:13 +0530

    added router for posting toDo

--- configs/middleware.js
+++ configs/middleware.js
@@ -1,13 +1,14 @@
 const jwtValidation = (request, response, next) => {
-  let token = request.headers.authorization;
+  const token = request.headers.authorization;
   if (token) {
     if (token.startsWith("Bearer ")) {
       request.headers.authorization = token.replace("Bearer ", "");
-      next();
+      return next();
     } else {
-      response.status(401).end();
+      return response.status(401).end();
     }
   }
+
   next();
 };
 
--- controllers/toDoRouter.js
+++ controllers/toDoRouter.js
@@ -1,4 +1,5 @@
 const toDoRouter = require("express").Router();
+const User = require("../models/user");
 const ToDo = require("../models/toDo");
 
 const jwt = require("jsonwebtoken");
@@ -10,7 +11,22 @@ toDoRouter.get("/todos/:user", async (request, response) => {
 });
 
 toDoRouter.post("/todos", async (request, response) => {
-  response.status(200).end();
+  const token = request.headers.authorization;
+  const username = jwt.verify(token, process.env.JWT_KEY);
+
+  const user = await User.findOne({ username: username });
+  if (user) {
+    const toDo = new ToDo({
+      toDo: request.body.toDo,
+      user: username,
+      isDone: false,
+    });
+
+    const newToDo = await toDo.save();
+    return response.json(newToDo);
+  }
+
+  response.status(400).end();
 });
 
 module.exports = toDoRouter;
