7a0c6cef317eb7c28b94f4b7febd2ced14b37b99
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-07-08 11:06:19 +0530

    implemented basic token authentication

--- controllers/loginRouter.js
+++ controllers/loginRouter.js
@@ -2,10 +2,16 @@ const loginRouter = require("express").Router();
 const User = require("../models/user");
 
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 loginRouter.post("/login", async (request, response) => {
   const user = request.body;
   const userInDB = await User.findOne({ username: user.username });
+
+  if (!userInDB) {
+    return response.status(400).json({ error: "user not found" });
+  }
 
   const isPasswordValid = await bcrypt.compare(
     user.password,
@@ -13,7 +19,8 @@ loginRouter.post("/login", async (request, response) => {
   );
 
   if (isPasswordValid) {
-    response.status(200).send(user);
+    const token = jwt.sign(userInDB.username, process.env.JWT_KEY);
+    response.status(200).json(token);
   }
   response.status(400).end();
 });
--- controllers/toDoRouter.js
+++ controllers/toDoRouter.js
@@ -1,10 +1,25 @@
 const toDoRouter = require("express").Router();
 const ToDo = require("../models/toDo");
 
+const jwt = require("jsonwebtoken");
+
 toDoRouter.get("/todos/:user", async (request, response) => {
   const user = request.params.user;
   console.log(user);
   response.status(200).end();
+});
+
+toDoRouter.post("/todos", async (request, response) => {
+  let token = request.headers.authorization;
+  if (token) {
+    if (token.startsWith("Bearer ")) {
+      token = token.replace("Bearer ", "");
+      const user = jwt.verify(token, process.env.JWT_KEY);
+      return response.json(user);
+    }
+  }
+
+  response.status(401).end();
 });
 
 module.exports = toDoRouter;
