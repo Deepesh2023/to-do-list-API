d9ad07f46a98e2323e84e1624204df820c7152df
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-07-10 11:08:06 +0530

    code refactor from index to app js

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const toDoRouter = require("./controllers/toDoRouter");
+const userRouter = require("./controllers/userRouter");
+const loginRouter = require("./controllers/loginRouter");
+const {
+  tokenCleanUp,
+  errorHandler,
+  decodeToken,
+} = require("./configs/middleware");
+
+const app = express();
+
+app.use(express.json());
+app.use("/api", loginRouter);
+app.use("/api", userRouter);
+app.use("/api", tokenCleanUp, decodeToken, toDoRouter);
+app.use(errorHandler);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const toDoRouter = require("./controllers/toDoRouter");
-const userRouter = require("./controllers/userRouter");
-const loginRouter = require("./controllers/loginRouter");
-const {
-  tokenCleanUp,
-  errorHandler,
-  decodeToken,
-} = require("./configs/middleware");
+const app = require("./app");
 
-const app = express();
 const port = 3000;
 
 mongoose.connect(process.env.MONGODB_URL);
-
-app.use(express.json());
-
-app.get("/", (request, response) => {
-  response.send("<h1>hello world</h1>");
-});
-
-app.use("/api", loginRouter);
-app.use("/api", userRouter);
-app.use("/api", tokenCleanUp, decodeToken, toDoRouter);
-app.use(errorHandler);
 
 app.listen(port, () => console.log("server started on port 3000"));
